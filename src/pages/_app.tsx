f08dddb5f481f9d0db257bab71b8ec5e3c499e15
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-11-14 23:15:03 +0900

    import Layouts component in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Layouts } from "@/components/layouts/layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layouts>
+      <Component {...pageProps} />
+    </Layouts>
+  );
 }
