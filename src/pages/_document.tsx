bb8b32fd537af41d1618ab448f1ccfb99250792b
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-11-14 22:49:39 +0900

    add: common <head> to _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { siteData } from "@/const/site";
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <meta property="og:locale" content={siteData.lang} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={siteData.title} />
+        <meta property="og:url" content={siteData.url} />
+        <meta property="og:site_name" content={siteData.title} />
+      </Head>
       <body>
         <Main />
         <NextScript />
