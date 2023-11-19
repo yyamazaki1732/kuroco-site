7401d528b26dc271de81a1ca766fb6a711883fd3
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-11-19 09:10:15 +0900

    add: props; 'customClass'

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,11 +1,11 @@
 import { Header, Footer } from "@/components/layouts/index";
 
-export const Layouts = ({ children }) => {
+export const Layouts = ({ customClass, children }) => {
   return (
-    <>
+    <div className={`${customClass}`}>
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
